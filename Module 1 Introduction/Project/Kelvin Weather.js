d913e6914d7bfff952dccd67a78aacd6d5796418
Suraj <S_pun@outlook.com>
2022-12-24 13:54:33 +0000

    Added Project1

---
+++ Module 1 Introduction/Project/Kelvin Weather.js
@@ -0,0 +1,15 @@
+// Forecast
+const kelvin = 293;
+
+// Celsius
+let celsius = kelvin - 273;
+
+// Fahrenheit
+let fahrenheit = Math.floor(celsius * (9 / 5) + 32);
+
+console.log(`The temerature is ${fahrenheit} degrees Fahrenheit.`);
+
+// Challenge
+let newton = Math.floor(celsius * (33 / 100));
+
+console.log(newton);
